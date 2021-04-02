a0f76c1f77a490d4a2a349c5c14ba4c50cc3abf0
Ankit Sagar <62764278+woinbo@users.noreply.github.com>
2021-04-03 03:37:02 +0530

    Update projects.dart

--- lib/config/projects.dart
+++ lib/config/projects.dart
@@ -2,13 +2,13 @@ import 'package:ankitsagar/models/project_model.dart';
 import 'assets.dart';
 
 final List<Project> projects = [
-  Project(
-      name: 'Doctropedia',
-      image: Assets.doctropedia,
-      description:
-          'Doctropedia is a tele-medicine app where user can call doctors and can take their health prescription of very light disease',
-      link:
-          'https://play.google.com/store/apps/details?id=in.doctropedia.doctropedia'),
+//   Project(
+//       name: 'Doctropedia',
+//       image: Assets.doctropedia,
+//       description:
+//           'Doctropedia is a tele-medicine app where user can call doctors and can take their health prescription of very light disease',
+//       link:
+//           'https://play.google.com/store/apps/details?id=in.doctropedia.doctropedia'),
   Project(
       name: 'Pigeon',
       image: Assets.pigeon,
